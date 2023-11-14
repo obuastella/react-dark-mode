7440fa486563bd04336c2f0001216bb69811373a
Stella <stellaobua123@gmail.com>
2023-11-14 06:13:07 +0100

    Initial Commit

---
+++ dark-mode/src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hello World!</h1>
+      <div className="box">
+        <h2>This is a box</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
