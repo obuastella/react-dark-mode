0fcce13f57199dea667e150a9d72c9daaf1b50f9
Stella <stellaobua123@gmail.com>
2023-11-16 03:13:50 +0100

    Finished writing the code

--- dark-mode/src/App.jsx
+++ dark-mode/src/App.jsx
@@ -1,9 +1,14 @@
 import "./index.css";
 import "./App.css";
-
+import useLocalStorage from "use-local-storage";
+import { Toggle } from "./components/Toggle";
 function App() {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <div className="App" data-theme="dark">
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
       <h1 className="title">Hello World!</h1>
       <div className="box">
         <h2>This is a box</h2>
